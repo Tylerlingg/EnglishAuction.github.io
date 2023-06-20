542dc81193242d6188e1c83413465592385f5f74
Ling <117559896+Tylerlingg@users.noreply.github.com>
2023-06-20 10:48:15 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,339 +1,79 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    // Check if Web3 is injected
-    if (typeof web3 !== 'undefined') {
-        window.web3 = new Web3(web3.currentProvider);
-    } else {
-        alert('Please install MetaMask');
-        return;
-    }
+// Connect to the Ethereum network using Web3.js or your preferred Ethereum library
 
-    // Set Contract ABI & Address
-    const contractABI = [
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "amountToken",
-				"type": "uint256"
-			}
-		],
-		"name": "addLiquidity",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
+// Contract address and ABI
+const contractAddress = '0x1bcAb0C9670441849d69dec353f86668fceFf70C';
+const contractABI = [
 	{
 		"inputs": [],
-		"name": "claimFees",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "contract IERC20",
-				"name": "_token",
-				"type": "address"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "user",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			}
-		],
-		"name": "FeesClaimed",
-		"type": "event"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "user",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amountETH",
-				"type": "uint256"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amountToken",
-				"type": "uint256"
-			}
-		],
-		"name": "LiquidityAdded",
-		"type": "event"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "user",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amountETH",
-				"type": "uint256"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amountToken",
-				"type": "uint256"
-			}
-		],
-		"name": "LiquidityRemoved",
-		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "liquidity",
-				"type": "uint256"
-			}
-		],
-		"name": "removeLiquidity",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "amountIn",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amountOutMin",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "maxSlippage",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "deadline",
-				"type": "uint256"
-			}
-		],
-		"name": "swap",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "user",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amountIn",
-				"type": "uint256"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "amountOut",
-				"type": "uint256"
-			}
-		],
-		"name": "Swapped",
-		"type": "event"
-	},
-	{
-		"stateMutability": "payable",
-		"type": "fallback"
-	},
-	{
-		"stateMutability": "payable",
-		"type": "receive"
-	},
-	{
-		"inputs": [],
-		"name": "accumulatedFees",
+		"name": "slot0",
 		"outputs": [
 			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "FEE_PERCENTAGE",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "getLatestPrice",
-		"outputs": [
-			{
-				"internalType": "int256",
-				"name": "",
-				"type": "int256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"name": "lastAccumulatedFees",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"name": "liquidityBalance",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "reserves",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "reserveETH",
-				"type": "uint256"
+				"internalType": "uint160",
+				"name": "sqrtPriceX96",
+				"type": "uint160"
 			},
 			{
-				"internalType": "uint256",
-				"name": "reserveToken",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "token",
-		"outputs": [
+				"internalType": "int24",
+				"name": "tick",
+				"type": "int24"
+			},
 			{
-				"internalType": "contract IERC20",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "totalLiquidity",
-		"outputs": [
+				"internalType": "uint16",
+				"name": "observationIndex",
+				"type": "uint16"
+			},
 			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
+				"internalType": "uint16",
+				"name": "observationCardinality",
+				"type": "uint16"
+			},
+			{
+				"internalType": "uint16",
+				"name": "observationCardinalityNext",
+				"type": "uint16"
+			},
+			{
+				"internalType": "uint8",
+				"name": "feeProtocol",
+				"type": "uint8"
 			}
 		],
 		"stateMutability": "view",
 		"type": "function"
 	}
-]; // Add contract ABI here
-    const contractAddress = '0xb178Cc6e3602Fc15E70e7fbfdA898f817d65E2e7'; // Add contract address here
+];
 
-    // Create Contract Instance
-    const pepeDexContract = new web3.eth.Contract(contractABI, contractAddress);
+// Instantiate the contract
+const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-    // Add event listeners
-    window.addLiquidity = async () => {
-        const amount = document.getElementById('add-liquidity-amount').value;
-        const accounts = await web3.eth.getAccounts();
-        pepeDexContract.methods.addLiquidity(amount).send({ from: accounts[0], value: amount });
-    };
+// Function to update the balance display
+async function updateBalance() {
+  const balance = await contract.methods.balanceOf(accountAddress).call();
+  document.getElementById('balance').textContent = balance;
+}
 
-    window.removeLiquidity = async () => {
-        const amount = document.getElementById('remove-liquidity-amount').value;
-        const accounts = await web3.eth.getAccounts();
-        pepeDexContract.methods.removeLiquidity(amount).send({ from: accounts[0] });
-    };
-
-    window.swap = async () => {
-        const amountIn = document.getElementById('swap-amount').value;
-        const amountOutMin = document.getElementById('amount-out-min').value;
-        constmaxSlippage = document.getElementById('max-slippage').value;
-        const deadline = document.getElementById('deadline').value;
-        const accounts = await web3.eth.getAccounts();
-        pepeDexContract.methods.swap(amountIn, amountOutMin, maxSlippage, deadline).send({ from: accounts[0], value: amountIn });
-    };
-
-    window.claimFees = async () => {
-        const accounts = await web3.eth.getAccounts();
-        pepeDexContract.methods.claimFees().send({ from: accounts[0] });
-    };
+// Event listener for the "Add Liquidity" button
+document.getElementById('add-liquidity-button').addEventListener('click', async () => {
+  const amountToken = document.getElementById('amount-token').value;
+  await contract.methods.addLiquidity(amountToken).send({ from: accountAddress, value: '0' });
+  updateBalance();
 });
+
+// Event listener for the "Swap" button
+document.getElementById('swap-button').addEventListener('click', async () => {
+  const amountIn = document.getElementById('amount-in').value;
+  const amountOutMin = document.getElementById('amount-out-min').value;
+  const maxSlippage = document.getElementById('max-slippage').value;
+  const deadline = Math.floor(Date.now() / 1000) + 3600; // Set the deadline to one hour from now
+  await contract.methods.swap(amountIn, amountOutMin, maxSlippage, deadline).send({ from: accountAddress });
+  updateBalance();
+});
+
+// Event listener for the "Claim Fees" button
+document.getElementById('claim-fees-button').addEventListener('click', async () => {
+  await contract.methods.claimFees().send({ from: accountAddress });
+  updateBalance();
+});
+
+// Initial balance update
+updateBalance();
